b68f320c12ce7cb8654383ecdd4b8f65deb7c848
jl <jlco42257@gmail.com>
2023-12-13 21:40:37 +0000

    luasnip and nerdtree installation.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("n", "<leader>telk", ":Telescope keymaps<CR>", { desc = "keymaps list." })
 vim.keymap.set("n", "<leader>upd", ":source %<CR>", { desc = "Update current file." })
+vim.keymap.set('n', '<c-n>', ':NERDTree<CR>', { desc = 'Nerd tree.' })
+vim.keymap.set('n', '<c-n>f', ':NERDTreeFind<CR>', { desc = 'Nerd tree find.' })
+vim.keymap.set('n', '<c-n>t', ':NERDTreeToggle<CR>', { desc = 'Nerd tree toggle.' })
+
